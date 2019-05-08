e9b2162bb701aa677156a283aae09eb3693e6ecc
Avery Liu <avery@Averys-MacBook-Pro.local>
2019-05-07 17:12:24 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+
+
+   render(){
+       return(
+           <header className="top-bar">
+               <div className="nav-box-wrapper"> 
+               <div className="top-bar_inner">
+
+               </div>
+               <nav className="nav-box">
+
+               </nav>
+               </div>
+
+           </header>
+       )
+   }
+
+}
+
+
+
+
+export default Header;
